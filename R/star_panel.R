6c85139552375f226c5e2da794430c7ccbf69fb7
Chandler Lutz <cl.eco@cbs.dk>
2016-12-27 22:58:55 -0700

    add fontface option to star_panel()

--- R/star_panel.R
+++ R/star_panel.R
@@ -12,14 +12,18 @@
 #'
 #' @param ... \code{stargazer} output
 #' @param starlist a list of \code{stargazer} output
-#' @param panel.names the names for the panels in the same
-#' order as the stargazer output
-#' @param reg (logical) set to TRUE if the panels are regression output;
-#' defualt is TRUE
+#' @param panel.names the names for the panels in the same order as
+#'     the stargazer output
+#' @param reg (logical) set to TRUE if the panels are regression
+#'     output; defualt is TRUE
 #' @param same.lhs.vars (logical) set to TRUE if all panels have the
-#' same left-hand-side variables. Currently, only TRUE is supported
-#' @param same.summary.stats (logical) set to TRUE if all panels have the
-#' same summary stats. Currently, only TRUE is supported
+#'     same left-hand-side variables. Currently, only TRUE is
+#'     supported
+#' @param same.summary.stats (logical) set to TRUE if all panels have
+#'     the same summary stats. Currently, only TRUE is supported
+#' @param panel.label.fontface The fontface for the panel
+#'     labels. Options are "plain", "italic", "bold",
+#'     "bold.italic". Default is "plain".
 #' @return the updated \code{stargazer} output
 #' @examples
 #' ## -- Regressoin example -- ##
@@ -44,8 +48,10 @@
 #' tex_write(star.panel.out, file = "my_tex_file.tex", headers = TRUE)
 #' }
 #' @export
-star_panel <- function(..., starlist = NULL, panel.names, reg = TRUE, same.lhs.vars = TRUE,
-                       same.summary.stats = TRUE) {
+star_panel <- function(..., starlist = NULL, panel.names,
+                       reg = TRUE, same.lhs.vars = TRUE,
+                       same.summary.stats = TRUE,
+                       panel.label.fontface = "plain") {
 
     ##Get all of the star output
     starlist <- c(list(...), starlist)
@@ -84,9 +90,24 @@ star_panel <- function(..., starlist = NULL, panel.names, reg = TRUE, same.lhs.v
                                   get_num_columns_tex(star.lines[[i]]),
                                   ##make sure to add an empty column after the
                                   ##panl name
-                                  "}{@{} l}{\\emph{", panel.names[i], "}} \n \\\\",
-                                  "\n \\\\[-1.5ex]")
+                                  "}{@{} l}{")
+        ##Add in the panel name with the fontface requested by the user
+        if (panel.label.fontface == "plain") {
+            temp.panel.name <- paste0(temp.panel.name, panel.names[i], "}")
+        } else if (panel.label.fontface == "bold") {
+            temp.panel.name <- paste0(temp.panel.name, "\\textbf{", panel.names[i], "}}")
+        } else if (panel.label.fontface == "italic") {
+            temp.panel.name <- paste0(temp.panel.name, "\\textit{", panel.names[i], "}}")
+        } else if (panel.label.fontface == "bold.italic") {
+            temp.panel.name <- paste0(temp.panel.name,
+                                      "\\textit{\\textbf{", panel.names[i], "}}}")
+        } else {
+            stop('temp.panel.name must be "plain", "bold", "italic", or "bold.italic')
+        }
 
+        ##Finish the line and add a little space
+        temp.panel.name <- paste0(temp.panel.name, "\n \\\\",
+                                  "\n \\\\[-1.5ex]")
 
         ##If not the last panel, add the hline and a little
         ##space after. Otherwise, add nothing
@@ -103,9 +124,6 @@ star_panel <- function(..., starlist = NULL, panel.names, reg = TRUE, same.lhs.v
                              )
 
         }
-
-
-
 
     out <- c(start.lines, unlist(star.lines), end.lines)
 
